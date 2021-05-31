cb60002f85c9193444dbb9aba8f377cbba5925c6
SirGuilhermeBR <sir_guilherme@hotmail.com>
2021-05-31 02:08:31 -0300

    add Stripe Escutador, API WebHook, Verificação de tudo que é tipo

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,22 +1,27 @@
 import { signIn, useSession } from 'next-auth/client'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
-
 interface SubscribeButtonProps{
     priceId: string;
 }
-
-
 export function SubscribeButton ({priceId}:SubscribeButtonProps){
     const [session] = useSession()
 
-    function handleSubscrible(){
+    async function handleSubscrible(){
         if(!session){
             signIn('github')
             return;
         }
-
+        try{
+            const response = await api.post('/subscribe');
+            const {sessionId} = response.data; 
+            const stripe = await getStripeJs()
+            await stripe.redirectToCheckout({sessionId})
+        }catch (err){
+            alert (err.message);
+        }
     }
-
     return (
         <button
             type="button"
